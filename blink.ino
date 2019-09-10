01ca95be63385234223d49f4673426eae1f8921f
jeewon9708 <jeewon9708@naver.com>
2019-09-10 22:26:11 +0900

    Update blink.ino

--- blink.ino
+++ blink.ino
@@ -1,1 +1,17 @@
 
+#include <Blink.h>
+
+
+Blink blink(4);
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  Serial.println("\nBlink Version 1.0 Jeewon Chae");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  blink.on(500);
+  blink.off(300);
+
+}
