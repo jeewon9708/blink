db06cb29364577bbaf2176f06df771a5cfc43909
jeewon9708 <jeewon9708@naver.com>
2019-09-10 22:25:46 +0900

    Update Blink.h

--- Blink.h
+++ Blink.h
@@ -1,1 +1,20 @@
 
+#ifndef Blink_h
+#define Blink_h
+
+#include "Arduino.h"
+
+class Blink
+{
+	public:
+		Blink(int pin);
+		void on(int delaytime);
+		void off(int delaytime);
+	private:
+		int _pin;
+};
+
+#endif
+
+
+
