ca2ac5d2c7b96ef504dfb46127069ac481a8ee28
jeewon9708 <jeewon9708@naver.com>
2019-09-10 22:25:24 +0900

    Update Blink.cpp

--- Blink.cpp
+++ Blink.cpp
@@ -1,1 +1,27 @@
+#include "Arduino.h"
+#include "Blink.h"
+
+Blink::Blink(int pin)
+{
+	_pin=pin;
+	pinMode(_pin,OUTPUT);
+	pinMode(0,OUTPUT);
+	digitalWrite(_pin,LOW);
+	
+}
+
+void Blink::on(int delaytime)
+{
+	delay(delaytime);
+	digitalWrite(_pin,HIGH);
+	Serial.println("high");
+	
+}
+void Blink::off(int delaytime)
+{	
+    delay(delaytime);
+	digitalWrite(_pin,LOW);
+	Serial.println("low");
+	
+}
 
